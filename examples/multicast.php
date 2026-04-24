6607e8fbc45d167dabc5f353e7f12c88216b93d9
sergey boyko <sergey@flyokai.com>
2026-04-24 13:02:28 -0400

    Initial flyokai/amp-data-pipeline release

---
+++ examples/multicast.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace {
+    include './bootstrap.php';
+}
+
+namespace Flyokai\AmpDataPipeline {
+
+    use Amp\Pipeline\ConcurrentIterator;
+    use Amp\Pipeline\Queue;
+    use Revolt\EventLoop;
+    use Flyokai\AmpDataPipeline\DataCast\CastProcessor;
+    use Flyokai\AmpDataPipeline\DataCast\MultiCastProcessor;
+    use Flyokai\AmpDataPipeline\DataItem\DataItem;
+    use Flyokai\AmpDataPipeline\DataItem\DataItemImpl;
+    use Flyokai\AmpDataPipeline\DataSource\ArraySource;
+    use Flyokai\AmpDataPipeline\Helper\ProcessorAssertion;
+
+    class ExtractColumns implements CastProcessor {
+        use ProcessorAssertion;
+        public function __construct(
+            public readonly array $columns
+        ) {
+        }
+
+        public function cast(ConcurrentIterator $source, \Closure $acceptCastItem): void
+        {
+            $this->assertSourceIterator($source);
+            /** @var DataItem $item */
+            foreach ($source as $item) {
+                $queue = new Queue(10);
+                $acceptCastItem($queue->iterate(), $item, $this);
+                $data = $item->getData();
+                foreach ($this->columns as $column) {
+                    $queue->push(DataItemImpl::fromArray([$data[$column] ?? null], [$column]));
+                }
+                $queue->complete();
+            }
+        }
+
+    }
+
+    function run()
+    {
+        $multicast = new MultiCastProcessor(
+            castProcessorFactories: [
+                fn() => new ExtractColumns([0,2]),
+                fn() => new ExtractColumns([3]),
+                fn() => new ExtractColumns([1,4]),
+            ],
+            groupResults: true,
+            groupBufferSize: 10
+        );
+
+        $composition = new ProcessorComposition([
+            $multicast->setConcurrency(5)
+        ]);
+
+        $input = [];
+
+        for ($i = 0; $i < 500; $i++) {
+            $input[] = DataItemImpl::fromArray(['aa-'.$i,'bb-'.$i,'cc-'.$i,'dd-'.$i,'ee-'.$i]);
+        }
+
+        $composition->setSource(new ArraySource($input));
+
+        foreach ($composition as $value) {
+            var_dump($value->getData());
+        }
+
+        $composition->reset();
+
+        echo "\n\n=========RESET========\n\n";
+
+        /*$composition->setSource(new ArraySource($input));
+
+        foreach ($composition as $value) {
+            var_dump($value->getData());
+        }*/
+    }
+
+    run();
+
+}
---
+++ src/DataCast/MultiCastProcessor.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace Flyokai\AmpDataPipeline\DataCast;
+
+use Amp\Future;
+use Amp\Pipeline\ConcurrentIterator;
+use Amp\Pipeline\Queue;
+use Flyokai\AmpDataPipeline\DataItem\DataItem;
+use function Amp\async;
+use function Amp\Future\await;
+use Flyokai\AmpDataPipeline\ProcessorAbstract;
+
+class MultiCastProcessor extends ProcessorAbstract
+{
+    /** @var \SplQueue<DataItem> */
+    protected \SplQueue $waitingQueue;
+
+    /**
+     * @param list<\Closure():CastProcessor> $castProcessorFactories
+     * @param \Closure():MultiCastConsumer|null $consumerFactory
+     */
+    public function __construct(
+        protected readonly array $castProcessorFactories,
+        protected bool           $groupResults = false,
+        protected int            $groupBufferSize = 0,
+        protected ?\Closure      $consumerFactory = null
+    )
+    {
+        $this->waitingQueue = new \SplQueue();
+        $this->consumerFactory ??= MultiCastConsumerImpl::selfCreate(...);
+    }
+
+    /**
+     * @param list<\Closure():CastProcessor> $castProcessorFactories
+     * @param \Closure():MultiCastConsumer|null $consumerFactory
+     */
+    public static function selfCreate(
+        array     $castProcessorFactories,
+        bool      $groupResults = false,
+        int       $groupBufferSize = 0,
+        ?\Closure $consumerFactory = null
+    ): static
+    {
+        return new self(
+            $castProcessorFactories,
+            $groupResults,
+            $groupBufferSize,
+            $consumerFactory
+        );
+    }
+
+    protected function read(ConcurrentIterator $iterator): void
+    {
+        $castProcessors = [];
+        /** @var \SplObjectStorage<CastProcessor, Queue> $castQueues */
+        $castQueues = new \SplObjectStorage();
+        /** @var \SplObjectStorage<CastProcessor, Future> $castFutures */
+        $castFutures = new \SplObjectStorage();
+        /** @var \SplObjectStorage<DataItem, \SplObjectStorage> $castResults */
+        $castResults = new \SplObjectStorage();
+        $waitingQueue = new \SplQueue();
+        foreach ($this->castProcessorFactories as $castProcessorFactory) {
+            $castProcessors[] = $castProcessorFactory();
+        }
+        foreach ($castProcessors as $processor) {
+            $castQueues[$processor] = $queue = new Queue($this->bufferSize);
+            $consumer = ($this->consumerFactory)(
+                processor: $processor,
+                queue: $queue,
+                castProcessors: $castProcessors,
+                castResults: $castResults,
+                waitingQueue: $waitingQueue,
+                releaseDataItem: $this->releaseDataItem(...),
+                groupResults: $this->groupResults,
+                groupBufferSize: $this->groupBufferSize
+            );
+            $castFutures[$processor] = async($consumer->consume(...));
+        }
+
+        $pushCastQueue = static function (CastProcessor $processor, Queue $castQueue, mixed $value): void {
+            $castQueue->push($value);
+        };
+        $completeCastQueue = static function (CastProcessor $processor, Queue $castQueue): void {
+            $castQueue->complete();
+        };
+
+        while ($iterator->continue($this->cancellation)) {
+            $value = $iterator->getValue();
+            $this->tapObjectStorage($castQueues, $pushCastQueue, $value);
+        }
+
+        $this->tapObjectStorage($castQueues, $completeCastQueue);
+
+        $castFutures = $this->collectObjectStorage(
+            $castFutures,
+            static fn(CastProcessor $processor, Future $castFuture): Future => $castFuture
+        );
+        await($castFutures);
+    }
+
+    protected function processDataItem(DataItem $value): void
+    {
+    }
+
+}
